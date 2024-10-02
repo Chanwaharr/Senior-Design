4903e8f3124312bead73cdf3f02ebc6c4653e4af
Chandler Harrison <exzoawsome1@gmail.com>
2024-10-02 18:50:09 -0500

    added comments

--- Hardware/src/main.cpp
+++ Hardware/src/main.cpp
@@ -47,7 +47,7 @@ double longitude = 0.0;
 
 // Global counter for people count
 volatile int PeopleCounter = 0;  // Use volatile because it's modified in an ISR
-const unsigned long sensor1Interval = 5000;  // 500 milliseconds (0.5 seconds)
+const unsigned long sensor1Interval = 5000;  // 5 seconds
 unsigned long previousMillisSensor1 = 0;    // Store last time sensor1 was read
 bool userChangedCounter = false;            // Flag to track if the user changed the PeopleCounter
 // Debounce time
@@ -368,7 +368,7 @@ void updateDisplay() {
   if (WiFi.status() == WL_CONNECTED) {
     display.println("WiFi connected");
   } else {
-    display.println("WiFi connection failed");
+    display.println("No WiFi");
   }
 
   display.display();
